55a2b84a2cd3272d91525fabcb07485b4a43c369
Kirill Davidenko <stafort777@gmail.com>
2023-02-04 19:29:04 +0300

    1 Task completed

--- task.js
+++ task.js
@@ -1,6 +1,20 @@
+"use strict"
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+  let discriminant = b ** 2 - 4 * a * c;
+  if (discriminant === 0) {
+    let root = -b / ( 2 * a );
+    arr = [];
+    arr.push(+root.toFixed(2));
+  } else if (discriminant > 0) {
+    let root1 = ( -b + Math.sqrt(discriminant) ) / ( 2 * a );
+    let root2 = ( -b - Math.sqrt(discriminant) ) / ( 2 * a );
+    arr = [];
+    arr.push(+root1.toFixed(2), +root2.toFixed(2));
+  } else {
+    arr = [];
+  }
   return arr; // array
 }
 
